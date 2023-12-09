e123b28012aa0c5badfd0445a78b4ca7ceeaa464
HikenCoding <erkandeg94@gmail.com>
2023-12-09 22:49:38 +0100

    read file from txt

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict'
+function readFromTxtFile(){
+    fetch("test.txt").then(function (res){
+        return res.text();
+    })
+    .then(function (data){
+        console.log(data);
+    })
+}
+
+console.log(readFromTxtFile())
