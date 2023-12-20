c636c094c8bc5899bdd358685a1bc27b740ae861
HikenCoding <erkandeg94@gmail.com>
2023-12-21 00:47:31 +0100

    checkpoint save

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 async function readFromTxtFile(){
     const res = await fetch("test.txt");
     const data = await res.text();
-    console.log(data)
+    // console.log(data)
     return data;
 }
 
@@ -16,20 +16,43 @@ async function readFromTxtFile(){
         let indexOfNumber=[];
 
         for(let i = 0; i < lines.length; i++){
-            for(let j = 0; j< lines[i].length-1; j++){
-                newChar =+ lines[i][j]
-                if(typeof newChar === "number")
-                {
-                    indexOfNumber = j
-                    console.log("DAS IST EINE ZAHL!!", j, newChar)
-                    console.log(indexOfNumber)
-                }
+            for(let j = 0; j<lines[i].length-1; j++){
+                console.log(lines[j], lines[i].length-1)
+                // newChar =+ lines[i][j]
+                // if(typeof newChar === "number")
+                // {
+                //     indexOfNumber = lines[j];
+                //     console.log(indexOfNumber);
+                //     containsNumber(indexOfNumber)
+                //     console.log(containsNumber(indexOfNumber))
+                //     // console.log(lines[j])
+                //     // console.log("DAS IST EINE ZAHL!!", j, newChar)
+                //     // console.log(indexOfNumber)
+                // }
             }
-            return newChar;
+            return indexOfNumber;
         }
     }
     catch(e){
         console.log("Whuppssss", e)
     }
 }
+
+function containsNumber(str) {
+    const match = str.match(/\d+/g);
+
+    if(match){
+        const firstNumber  = parseInt(match[0],10);
+        const lastNumber = parseInt(match[match.length-1],10);
+        
+        return {
+            firstNumber: firstNumber,
+            lastNumber: lastNumber
+    };
+}
+    else{
+        return null;
+    }
+}
+
 getNumbersFromLines()
