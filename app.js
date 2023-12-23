50e4ba97ec5088858ba7b43e03a74c4924a16549
HikenCoding <erkandeg94@gmail.com>
2023-12-23 03:33:51 +0100

    getEntriesFromObject function is working

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 'use strict'
 
-const compareArr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+const compareArr = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+};
 
 async function readFromTxtFile(){
     const res = await fetch("test.txt");
@@ -16,19 +26,18 @@ async function readFromTxtFile(){
         let newArray = [];
 
         lines.forEach(line => {
+            console.log(line, "LINE!!!")
             const numbersInLine = line.match(/\d+/g);
-            if(numbersInLine){
                 if(numbersInLine[0] === numbersInLine[numbersInLine.length-1]){
                     newArray.push(numbersInLine[0]+numbersInLine[0]);
                 }
                 else {
                     newArray.push(numbersInLine[0]+numbersInLine[numbersInLine.length-1])
                 }   
-            }
             
         });
         
-        const sum = Object.values(newArray).reduce((accumulator, currentValue) => accumulator + parseFloat(currentValue), 0);
+        const sum = newArray.reduce((accumulator, currentValue) => accumulator + parseFloat(currentValue), 0);
         console.log(`Numbers from line: ${newArray} \n The summary of these are: ${sum}`)
 
     }
@@ -37,7 +46,43 @@ async function readFromTxtFile(){
     }
 }
 
-getNumbersFromLines()
+// getNumbersFromLines()
+
+async function getWordsFromArray(compareArr) {
+try {
+    const readTxt = await readFromTxtFile();
+    const lines = readTxt.split('\n');
+
+
+    const result = Object.keys(compareArr).every(substring =>
+        lines.some(element => element.includes(substring))
+        );
+        console.log(result)
+
+        const isFirstElementMatched = Object.keys(compareArr).some(substring => lines[0].includes(substring))
+        console.log(isFirstElementMatched)
+        // console.log(line.includes(compareArr))
+
+} catch (e) {
+    console.log("This is an Error", e)
+}
+}
+
+
+const getEntriesFromObject = function (compareArr){
+    let example = "twotestda2taon3esix"
+    for(const key in compareArr) {
+        if(compareArr.hasOwnProperty(key)){
+            const regex = new RegExp(key, 'g');
+            example = example.replace(regex, compareArr[key])
+        }
+    }
+
+    console.log(example)
+}
+
+getEntriesFromObject(compareArr)
+
 
 
 
