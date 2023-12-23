3a78bc839a8b147e35468866e9b1d6713a9da7bc
HikenCoding <erkandeg94@gmail.com>
2023-12-23 04:57:14 +0100

    Solution Part 1

--- app.js
+++ app.js
@@ -1,88 +1,76 @@
 'use strict'
 
-const compareArr = {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-};
+// const compareArr = {
+//     one: 1,
+//     two: 2,
+//     three: 3,
+//     four: 4,
+//     five: 5,
+//     six: 6,
+//     seven: 7,
+//     eight: 8,
+//     nine: 9,
+// };
 
-async function readFromTxtFile(){
-    const res = await fetch("test.txt");
-    const data = await res.text();
-    console.log(data)
-    return data;
-}
+// async function readFromTxtFile(){
+//     const res = await fetch("test.txt");
+//     const data = await res.text();
+//     console.log(data)
+//     return data;
+// }
 
- async function getNumbersFromLines(){
-    try{
-        const readTxt = await readFromTxtFile();
-        const lines = readTxt.split('\n');
-        let newArray = [];
 
-        lines.forEach(line => {
-            console.log(line, "LINE!!!")
-            const numbersInLine = line.match(/\d+/g);
-                if(numbersInLine[0] === numbersInLine[numbersInLine.length-1]){
-                    newArray.push(numbersInLine[0]+numbersInLine[0]);
-                }
-                else {
-                    newArray.push(numbersInLine[0]+numbersInLine[numbersInLine.length-1])
-                }   
-            
-        });
+
+ 
+
+
+
+// const getEntriesFromObject = async function (compareArr){
+//     try {
+//     const readTxt = await readFromTxtFile();
+//     let examples = readTxt.split('\n');
+//     let newArray = [];
+//     examples.forEach(example => {
+//         for(const key in compareArr) {
+//             if(compareArr.hasOwnProperty(key)){
+//                 const regex = new RegExp(key, 'g');
+//                 example = example.replace(regex, parseFloat(compareArr[key]))
+//                 console.log(example, "OLD ONE")
+//             }
+//         }
+//         const numbersInLine = example.match(/([+-]?\d+)/g);
+//         console.log(numbersInLine, "NUMBERS IN LINEEE")
+
+//         if(numbersInLine[0] === numbersInLine[numbersInLine.length-1]){
+//             newArray.push(numbersInLine[0]+numbersInLine[0]);
+//         }
+//         else if(numbersInLine[0] !== numbersInLine[numbersInLine.length-1]){
+//             newArray.push(numbersInLine[0]+numbersInLine[numbersInLine.length-1])
+//         }   
+
         
-        const sum = newArray.reduce((accumulator, currentValue) => accumulator + parseFloat(currentValue), 0);
-        console.log(`Numbers from line: ${newArray} \n The summary of these are: ${sum}`)
+//     });
+//     const sum = newArray.reduce((accumulator, currentValue) => accumulator + parseFloat(currentValue), 0);
+//     console.log(`Numbers from line: ${newArray} \n The summary of these are: ${sum}`)
 
-    }
-    catch(e){
-        console.log("Whuppssss", e)
-    }
-}
+//     } catch (e) {
+//         console.log("THIS IS A FREAKING ERROR", e)
+//     }
+    
+// }
 
-// getNumbersFromLines()
-
-async function getWordsFromArray(compareArr) {
-try {
-    const readTxt = await readFromTxtFile();
-    const lines = readTxt.split('\n');
+// getEntriesFromObject(compareArr)
 
 
-    const result = Object.keys(compareArr).every(substring =>
-        lines.some(element => element.includes(substring))
-        );
-        console.log(result)
+let fs = require('fs');
+let input= fs.readFileSync('test.txt').toString();
 
-        const isFirstElementMatched = Object.keys(compareArr).some(substring => lines[0].includes(substring))
-        console.log(isFirstElementMatched)
-        // console.log(line.includes(compareArr))
+let total = 0;
 
-} catch (e) {
-    console.log("This is an Error", e)
-}
-}
-
-
-const getEntriesFromObject = function (compareArr){
-    let example = "twotestda2taon3esix"
-    for(const key in compareArr) {
-        if(compareArr.hasOwnProperty(key)){
-            const regex = new RegExp(key, 'g');
-            example = example.replace(regex, compareArr[key])
+        for(let line of input.split('\n')) {
+            const numbers = line.split('').filter(el => !isNaN(parseInt(el)));
+            total += parseInt(`${numbers[0]}${numbers[numbers.length-1]}`);
         }
-    }
 
-    console.log(example)
-}
-
-getEntriesFromObject(compareArr)
-
-
-
-
+        
+        console.log(total)
